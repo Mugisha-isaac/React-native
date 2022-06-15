e8384e44b8cb58f5a126a9aa8ce561cd2e0dab51
Mugisha-isaac <didiermunezer38@gmail.com>
2022-06-16 01:15:05 +0200

    feat: flat list

--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,25 @@
+import { useState } from "react"
+import { View,SafeAreaView,Text,FlatList } from "react-native-web";
+import {COLORS,NFTData} from '../constants';
+import {NFTCard,FocusedStatusBar,HomeHeader} from '../components'
+
+
 
 
 const Home = ()=>{
     return(
-       "Home"
+       <SafeAreaView style={{flex:1}}>
+            <FocusedStatusBar background ={COLORS.primary} />
+
+            <View style={{flex:1}}>
+                <View style={{zIndex:0}}>
+                      <FlatList
+                        data={NFTData}
+                        renderItem={({item})=><Text> {item.name}</Text>}
+                      />
+                </View>
+            </View>
+       </SafeAreaView>
     )
 }
 
